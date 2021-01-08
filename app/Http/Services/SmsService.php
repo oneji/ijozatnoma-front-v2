55cfbf6bbdd485d46fa00cff7b185460e2133448
Tim Kamilov <timich1995@gmail.com>
2021-01-08 17:40:16 +0500

    NEW: Check if sms code has been set to the phone number.

--- app/Http/Services/SmsService.php
+++ app/Http/Services/SmsService.php
@@ -15,15 +15,23 @@ class SmsService
     public function send($phoneNumber)
     {
         $now = Carbon::now();
-        $randomCode = mt_rand(100000, 999999);
-        # sms logic
-        $smsCode = new SmsCode();
-        $smsCode->phone_number = $phoneNumber;
-        $smsCode->code = $randomCode; // Test code
-        $smsCode->sent_at = $now;
-        $smsCode->expires_at = Carbon::parse($now)->addMinutes(3);
-        $smsCode->save();
+        $threeMinsLater = Carbon::parse($now)->addMinutes(3);
 
-        return $smsCode;
+        $oldCodes = SmsCode::wherePhoneNumber($phoneNumber)
+            ->whereActive(1)
+            ->where('expires_at', '>', $now)
+            ->get();
+        
+        $smsCode = null;
+        if($oldCodes->count() === 0) {
+            $randomCode = mt_rand(100000, 999999);
+            # sms logic
+            $smsCode = new SmsCode();
+            $smsCode->phone_number = $phoneNumber;
+            $smsCode->code = $randomCode;
+            $smsCode->sent_at = $now;
+            $smsCode->expires_at = $threeMinsLater;
+            $smsCode->save();
+        }
     }
 }
